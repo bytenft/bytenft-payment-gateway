901b459a9ca7b1ab2b20fca02f855cf20ccca23c
Hiren Master <hiren@rethinkpay.com>
2025-07-10 14:54:06 +0530

    update live url

--- includes/config.php
+++ includes/config.php
@@ -5,8 +5,7 @@ if (!defined('BYTENFT_PROTOCOL')) {
 }
 
 if (!defined('BYTENFT_HOST')) {
-    // define('BYTENFT_HOST', 'byte-nft.lcl');
-    define('BYTENFT_HOST', '127.0.0.1:8000');
+    define('BYTENFT_HOST', 'www.bytenft.xyz');
 }
 
 if (!defined('BYTENFT_BASE_URL')) {
