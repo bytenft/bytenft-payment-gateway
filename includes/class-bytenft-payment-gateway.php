c3b9b298f5c0e3b9e7bdad2c2c820c0b8b60ae57
Vimal Dudhat <vimal@rethinkpay.com>
2025-07-25 13:46:55 +0530

    In Active Account Fix

--- includes/class-bytenft-payment-gateway.php
+++ includes/class-bytenft-payment-gateway.php
@@ -1164,6 +1164,58 @@ class BYTENFT_PAYMENT_GATEWAY extends WC_Payment_Gateway_CC
 		]);
 	}
 
+	public function get_updated_account() {
+		$accounts = get_option('woocommerce_bytenft_payment_gateway_accounts', []);
+		$valid_accounts = [];
+		foreach ($accounts as $index => $account) {
+		    $useSandbox = $this->sandbox;
+		    $secretKey = $useSandbox ? $account['sandbox_secret_key'] : $account['live_secret_key'];
+		    $publicKey = $useSandbox ? $account['sandbox_public_key'] : $account['live_public_key'];
+
+		    $this->log_info("Checking merchant status for account #$index", [
+		        'context' => compact('useSandbox', 'publicKey')
+		    ]);
+
+		    $checkStatusUrl = $this->get_api_url('/api/check-merchant-status', $useSandbox);
+
+		    $response = wp_remote_post($checkStatusUrl, [
+		        'headers' => [
+		            'Authorization' => 'Bearer ' . $publicKey,
+		            'Content-Type'  => 'application/json',
+		        ],
+		        'timeout' => 10,
+		        'body' => wp_json_encode([
+		            'api_secret_key' => $secretKey,
+		            'is_sandbox'     => $useSandbox,
+		        ]),
+		    ]);
+
+		    $body = json_decode(wp_remote_retrieve_body($response), true);
+			$hasError = is_array($body) && strtolower($body['status'] ?? '') === 'error';
+	
+			$valid_accounts[$index] = [
+				'title' => $account['title'],
+				'priority' => $account['priority'],
+				'live_public_key' => $account['live_public_key'],
+				'live_secret_key' => $account['live_secret_key'],
+				'sandbox_public_key' => $account['sandbox_public_key'],
+				'sandbox_secret_key' => $account['sandbox_secret_key'],
+				'has_sandbox' => $account['has_sandbox'],
+				'sandbox_status' => $hasError ? 'Inactive' : 'Active',
+				'live_status' => $hasError ? 'Inactive' : 'Active',
+			];
+			$index++;
+		    $this->log_info("Account #$index not active", ['context' => $body]);
+		}
+
+		if (!empty($valid_accounts)) {
+			update_option('woocommerce_bytenft_payment_gateway_accounts', $valid_accounts);
+		}
+
+	    $this->log_info('No active account. Removing bytenft gateway.');
+	    return false;
+	}
+
 	public function get_active_account() {
 	    $cache_key = 'bytenft_active_payment_account';
 	    $cached_account = get_transient($cache_key);
@@ -1238,6 +1290,7 @@ class BYTENFT_PAYMENT_GATEWAY extends WC_Payment_Gateway_CC
 
 	    if (is_admin()) {
 	        $this->log_info('In admin area, skipping gateway check.');
+			$this->get_updated_account();
 	        return $available_gateways;
 	    }
 
