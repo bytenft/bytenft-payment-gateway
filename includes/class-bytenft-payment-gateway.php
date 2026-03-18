dcf141554da5e3a1dacc5ae974d72dbd3744c945
Vimal Dudhat <vimal@rethinkpay.com>
2026-03-18 19:49:50 +0530

    Remove daily limit check code

--- includes/class-bytenft-payment-gateway.php
+++ includes/class-bytenft-payment-gateway.php
@@ -1448,17 +1448,17 @@ class BYTENFT_PAYMENT_GATEWAY extends WC_Payment_Gateway_CC
 			return $this->hide_gateway($available_gateways, $gateway_id);
 		}
 
-		if ($all_accounts_limited) {
-			$this->log_info_once_per_session(
-				'accounts_limited_' . $cart_hash,
-				'ByteNFT payment option hidden: all accounts have reached their transaction limits'
-			);
-			$this->log_info_once_per_session(
-				'gateway_check_end_' . $cart_hash,
-				'ByteNFT payment option check finished'
-			);
-			return $this->hide_gateway($available_gateways, $gateway_id);
-		}
+		// if ($all_accounts_limited) {
+		// 	$this->log_info_once_per_session(
+		// 		'accounts_limited_' . $cart_hash,
+		// 		'ByteNFT payment option hidden: all accounts have reached their transaction limits'
+		// 	);
+		// 	$this->log_info_once_per_session(
+		// 		'gateway_check_end_' . $cart_hash,
+		// 		'ByteNFT payment option check finished'
+		// 	);
+		// 	return $this->hide_gateway($available_gateways, $gateway_id);
+		// }
 
 		$this->log_info_once_per_session(
 			'gateway_active_' . $cart_hash,
