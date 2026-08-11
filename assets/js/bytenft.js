1cec34bd04b4409dab574b75f41eb6baad0cf086
hiren-master <133195932+hiren-master@users.noreply.github.com>
2026-08-11 22:53:24 +0300

    Revert "updated plugin log sending calls"

--- assets/js/bytenft.js
+++ assets/js/bytenft.js
@@ -802,12 +802,7 @@
 
                             return;
                         }
-                            
-                        if (response?.data?.reload) {
-                            window.location.reload();
-                            return;
-                        }
-                        
+
                         // Payment cancelled/failed
                         self.cleanupPopup();
 
